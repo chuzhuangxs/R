bdaf10bda214550be8f31b9a7627bb7826327558
chuzhuangxs <chuzhuangxs@outlook.com>
2017-05-17 18:39:19 +0800

    update

--- learn/tiedaofilter.R
+++ learn/tiedaofilter.R
@@ -8,7 +8,15 @@ library(RColorBrewer)
 data<-read.xlsx("E:/R/R_graphics_cookbook/learn/tiedao1.xlsx", stringsAsFactors=FALSE,header=TRUE,encoding = "UTF-8",sheetIndex = 1)
 #生成语料库
 tiedao<-VCorpus(VectorSource(data$comment))
+reuters<-tm_map (tiedao,stripWhitespace)
+reuters<-tm_map(reuters,removeWords,stopwords("english"))
+tm_map(reuters,stemDocument)
+dtm<-DocumentTermMatrix(reuters)
+
 #开启人名识别
 segment.options(isNameRecognition=TRUE)
 getOption("isNameRecognition")
+data1<-segmentCN(reuters)
+
+str(data1)
 tiedao
