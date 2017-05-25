c8ffbf73a1b889124342e5fa4cbcf6f8dc788b8e
chuzhuangxs <chuzhuangxs@outlook.com>
2017-05-25 22:45:34 +0800

    描述性统计分析

--- descriptive.R
+++ descriptive.R
@@ -15,3 +15,98 @@ ggplot(faithful,aes(x=waiting))+geom_histogram(binwidth = binsize,fill="white",c
 h<-ggplot(faithful,aes(x=waiting))
 h+geom_histogram(binwidth = 8,fill="white",colour="black",boundary=31)
 h+geom_histogram(binwidth = 8,fill="white",colour="black",boundary=35)
+#基于分组数据绘制分组直方图
+
+ggplot(birthwt,aes(x=bwt))+geom_histogram(fill="white",colour="black")+facet_grid(smoke~.)
+birthwt
+ggplot(birthwt,aes(x=bwt))+geom_histogram(fill="white",colour="black")+facet_grid(race~.)
+#改变分组数据
+birthwt1<-birthwt
+birthwt1$smoke<-factor(birthwt1$smoke)
+levels(birthwt1$smoke)
+#重置标签
+birthwt1$smoke<-revalue(birthwt1$smoke,c("0"="No Smoke","1"="Smoke"))
+ggplot(birthwt1,aes(x=bwt))+geom_histogram(fill="white",colour="black")+facet_grid(smoke~.)
+ggplot(birthwt,aes(x=bwt))+geom_histogram(fill="white",colour="black")+facet_grid(race~.)
+#单独设置y轴标度
+ggplot(birthwt,aes(x=bwt))+geom_histogram(fill="white",colour="black")+facet_grid(race~.,scales = "free")
+#另一种分组方法，把分组变量映射给fill,没有position = "identity"，直方图会被堆积
+ggplot(birthwt1,aes(x=bwt,fill=smoke))+geom_histogram(position = "identity",alpha=0.4)
+#绘制密度曲线
+ggplot(faithful,aes(x=waiting))+geom_density()
+ggplot(faithful,aes(x=waiting))+geom_line(stat = "density")+expand_limits(y=0)
+#快速绘制未在数据框内的数据图像
+w<-faithful$waiting
+ggplot(NULL,aes(x=w))+geom_density()
+#设置密度曲线的带宽，使用adjust参数
+ggplot(faithful,aes(x=waiting))+geom_line(stat = "density",adjust=0.25,colour="red")+geom_line(stat = "density")+geom_line(stat = "density",adjust=2,colour="blue")
+#手动设定X轴的范围,使用xlim函数
+ggplot(faithful,aes(x=waiting))+geom_density(fill="blue",alpha=0.2)+xlim(35,105)
+#设置y=..density..使密度曲线与直方图的标度相对
+ggplot(faithful,aes(x=waiting,y=..density..))+geom_histogram(fill="cornsilk",colour="grey60",size=0.2)+geom_density()+xlim(35,105)
+#基于分组数据绘制分组密度曲线
+str(birthwt1)
+ggplot(birthwt1,aes(x=bwt,colour=smoke))+geom_density()
+ggplot(birthwt1,aes(x=bwt,fill=smoke))+geom_density(alpha=0.3)
+#可以使用分面
+ggplot(birthwt1,aes(x=bwt))+geom_density(fill="blue",alpha=0.3)+facet_grid(smoke~.)
+#加上直方图
+ggplot(birthwt1,aes(x=bwt,y=..density..))+geom_histogram(binwidth = 200,fill="cornsilk",colour="grey60",size=0.2)+geom_density()+facet_grid(smoke~.)
+#绘制频数多边形
+ggplot(faithful,aes(x=waiting))+geom_freqpoly()
+ggplot(faithful,aes(x=waiting))+geom_freqpoly(binwidth=4)
+#绘制箱线图
+ggplot(birthwt,aes(x=factor(race),y=bwt))+geom_boxplot()
+#修改箱线图的宽度
+ggplot(birthwt,aes(x=factor(race),y=bwt))+geom_boxplot(width=0.5)
+#修改异常点的大小和点形
+ggplot(birthwt,aes(x=factor(race),y=bwt))+geom_boxplot(outlier.size = 1.5,outlier.shape = 21)
+#单组数据箱线图
+ggplot(birthwt,aes(x=1,y=bwt))+geom_boxplot()+scale_x_continuous(breaks = NULL)+theme(axis.title.x = element_blank())
+#向箱线图添加槽口
+ggplot(birthwt,aes(x=factor(race),y=bwt))+geom_boxplot(notch = TRUE)
+ggplot(birthwt,aes(x=factor(race),y=bwt))+geom_boxplot(notch = FALSE)
+#向箱线图添加均值
+ggplot(birthwt,aes(x=factor(race),y=bwt))+geom_boxplot()+stat_summary(fun.y = "mean",geom = "point",shape=23,size=3,fill="white")
+#绘制小提琴图
+p<-ggplot(heightweight,aes(x=sex,y=heightIn))
+p+geom_violin()
+#添加箱线图
+p+geom_violin(fill="red",alpha=0.4)+geom_boxplot(width=0.1,fill="blue",alpha=0.3,outlier.colour = NA)+stat_summary(fun.y = median,geom = "point",fill="white",shape=21,size=2.5)
+#设置保留尾部
+p+geom_violin(fill="lightblue",alpha=0.4,trim=FALSE)
+#校准小提琴图的面积
+p+geom_violin(fill="lightblue",alpha=0.4,trim=FALSE,scale = "count")
+#调整平滑程度
+p+geom_violin(fill="lightblue",alpha=0.4,trim=FALSE,scale = "count",adjust=2)
+#绘制Wilkinson点图
+countries2009<-subset(countries,Year==2009&healthexp>2000)
+p<-ggplot(countries2009,aes(x=infmortality))
+p+geom_dotplot()
+#移除无意义的Y轴
+p+geom_dotplot(binwidth = 0.25)+geom_rug()+scale_y_continuous(breaks = NULL)+theme(axis.title.y = element_blank())
+#固定间距的分组
+p+geom_dotplot(binwidth = 0.25,method = "histodot")+geom_rug()+scale_y_continuous(breaks = NULL)+theme(axis.title.y = element_blank())
+#中心堆叠
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
